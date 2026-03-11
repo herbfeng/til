b73a898f4a60ae410d93c492d35673e93e2b0407
Herb <hi@herbfeng.com>
2026-03-10 19:15:48 -0700

    Initial Commit

---
+++ TIL/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TIL
+//
+//  Created by Herb on 3/10/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TIL/TILApp.swift
@@ -0,0 +1,32 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by Herb on 3/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TILApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
